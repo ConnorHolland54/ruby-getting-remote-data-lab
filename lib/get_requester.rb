c62f1150232d34d999f09c557c8c0433e497c21e
Connor Holland <supersoccergoalie123@gmail.com>
2020-12-20 21:39:03 -0700

    commit tree 4305ca008c8400dc9812f6b5a75229188c8355d3

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,32 @@
 # Write your code here
+require 'open-uri'
+require 'net/http'
+require 'json'
+
+class GetRequester
+
+    attr_accessor :url
+
+
+    def initialize(url)
+        @url = url
+    end
+
+    def get_response_body
+        uri = URI.parse(@url)
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+
+    def parse_json
+        people_array = []
+        people = JSON.parse(self.get_response_body)
+        people.collect do |people|
+            people_array << {"name" => people["name"], "occupation" => people["occupation"]}
+        end
+        people_array
+
+    end
+
+
+end
